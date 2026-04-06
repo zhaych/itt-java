b5841e59f90721b05547bce4bd16f62110d91e81
zhaychtattoos <zhaychtattoos@gmail.com>
2026-04-06 21:13:36 +0300

    Homework L3 task Six

--- Homework_L3/taskSix.java
+++ Homework_L3/taskSix.java
@@ -1,7 +1,21 @@
 package Homework_L3;
 
+import java.util.Scanner;
+
 public class taskSix {
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input number: ");
+
+        int a = input.nextInt();
+        int i = 1;
+        int temp = 0; 
+
+        for (i = 1; i <= a; i++){
+            temp = temp + i; 
+        }
+        System.out.println(temp);
     }
 }
